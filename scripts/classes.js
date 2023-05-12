1c0a4f470dd2e3e955830d4e0bb35e661a26753d
XCPika <61687817+XCPika@users.noreply.github.com>
2023-05-12 13:18:36 +0100

    All class data generated. Need to fix page scaling

--- scripts/classes.js
+++ scripts/classes.js
@@ -12,12 +12,37 @@ initializePage = (function(_super) {
 
 function initializeClasses() {
     var current_class = self.location.search.substring(1);
-    const current_class_element = $("#current_class")
-    current_class_element.children(".class-banner-title").text(current_class.charAt(0).toUpperCase() + current_class.slice(1));
-    current_class_element.children(".class-banner-icon").css("background-image", `url(./assets/img/icons/${current_class}.png)`)
+    var class_data;
+    if (current_class != "") {
+        const current_class_element = $("#current_class");
+        class_data = data[current_class];
+        current_class_element.children(".class-banner-title").text(class_data.name);
+        current_class_element.children(".class-banner-icon").css("background-image", `url(./assets/img/icons/${current_class}.png)`);
+        current_class_element.append($("<div/>").addClass(`class-banner-impact impact-${class_data.impact}`));
+        const class_details_element = $("#class-details").children(".main-class-details");
+        class_details_element
+            .append($("<h2/>").text("Description:"))
+            .append($("<p/>").text(class_data.description))
+            .append($("<h2/>").text("Powers:"))
+            .append($("<p/>").text(`Activation Key: ${class_data.key}`));
+        var power_data;
+        for (const key in class_data.powers) {
+            if (Object.hasOwnProperty.call(class_data.powers, key)) {
+                power_data = class_data.powers[key];
+                class_details_element
+                    .append($("<h3/>").text(`${key}`))
+                    .append($("<p/>").text(`${power_data.description}`))
+                    .append($("<p/>").text(`Active Ability: ${power_data.active}`))
+                if (power_data.active) {
+                    class_details_element
+                        .append($("<p/>").text(`Cooldown: ${power_data.cooldown}`))
+                };
+            }
+        }
+    }
     for (const key in data) {
         if (Object.hasOwnProperty.call(data, key)) {
-            const class_data = data[key];
+            class_data = data[key];
             $("#class-view").append(
                 $("<div/>")
                 .attr("role", "button")
@@ -26,9 +51,7 @@ function initializeClasses() {
                 .addClass("class-banner pixelated")             
                 .append($("<div/>").addClass("class-banner-icon pixelated").css("background-image", `url(./assets/img/icons/${key}.png)`))
                 .append($("<p/>").html(class_data.name).addClass("class-banner-title"))
-                .append($("<div/>").addClass("class-banner-impact-blank"))
-                .append($("<div/>").addClass("class-banner-impact-blank"))
-                .append($("<div/>").addClass("class-banner-impact-blank"))
+                .append($("<div/>").addClass(`class-banner-impact impact-${class_data.impact}`))
             )
         }
     };
