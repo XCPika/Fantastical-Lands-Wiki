79c30bf27a9d8e9f3c518c62b50b75a8d0bbde89
XCPika <61687817+XCPika@users.noreply.github.com>
2024-01-29 19:13:45 +0000

    Lighthouse testing and improvements. Started item page
    Switched common use images to webp from png.
    Fixed more css issues to prepare for mobile friendly layout.

--- scripts/index.js
+++ scripts/index.js
@@ -31,3 +31,49 @@ function setDarkMode(mode) {
     document.body.className = mode ? "dark-mode": "";
     document.getElementById("dark-mode-toggle").src = mode ? "./assets/img/page/dark_mode/light-mode.png" : "./assets/img/page/dark_mode/dark-mode.png";
 }
+
+
+/**
+ * MODIFIED VERSION OF getURL FROM: https://gomakethings.com/ditching-jquery/#get-html-from-another-page
+ * 
+ * Get HTML from another URL
+ * @param  {String}   url         The URL
+ * @param  {String}   findElem    Query selector of the element to replace
+ * @param  {Function} error       Callback on failure
+ */
+
+async function getURL(url, findElem, error ) {
+	// Feature detection
+	if ( !window.XMLHttpRequest ) return;
+	// Create new request
+	var request = new XMLHttpRequest();
+
+    let promise = new Promise(
+        function (resolve, reject) {
+            // Setup callbacks
+            request.onreadystatechange = function () {
+                // If the request is complete
+                if ( request.readyState === 4 ) {
+                    // If the request failed
+                    if ( request.status !== 200 ) {
+                        if ( error && typeof error === 'function' ) {
+                            error( request.responseText, request );
+                        }
+                        reject(request.responseText);
+                    }
+                    // If the request succeeded
+                    resolve(request.responseText)
+                }
+            };
+
+            // Get the HTML
+            request.open( 'GET', url );
+            request.send();
+        }
+    )
+    
+    var embed = document.querySelector(findElem);
+    if (!embed) return;
+    embed.outerHTML = await promise;
+	
+};
